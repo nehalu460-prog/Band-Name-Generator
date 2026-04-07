73704c981e389620d49cb2cf4fe80311b6b4fd4f
nehalu460-prog <nehalu460@gmail.com>
2026-04-07 16:59:37 +0530

    Added day2 code

---
+++ day3/1st.py
@@ -0,0 +1,21 @@
+print("Welcome to the python pizza deliveries:")
+size = input("what size of pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N:")
+extra_cheese = input("Do you want extra chesse? Y or N: ")
+bill = 0
+if size =="S":
+    bill = 15
+elif size =="M":
+    bill = 20
+elif size == "L":
+    bill = 25
+else:
+    print("You typed the wrong inputs")
+if pepperoni == "Y":
+    if size == "S":
+        bill+=2
+    else:
+        bill+=3
+if extra_cheese =="Y":
+    bill+=1
+print(f"Your final bill is: $ {bill}.")
