0621bc1688492f7ec05ca34fe905540afa01849c
nehalu460-prog <nehalu460@gmail.com>
2026-06-17 21:43:15 +0530

    Day 10 Simple Calculator

--- day10/3rd.py
+++ day10/3rd.py
@@ -1,13 +1,49 @@
-def my_function(name, section):
-    if name == "" or section == "":
-        return "You did not provide valid inputs"
+def add(n1, n2):
+    return n1 + n2
 
-    final_name = name.title()
-    final_section = section.title()
 
-    return f"{final_name} {final_section}"
+def subtract(n1, n2):
+    return n1 - n2
 
-name = input("What is your name? ")
-section = input("Which section are you from? ")
 
-print(my_function(name, section))
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+# print(operations["*"](4, 8))
+
+
+def calculator():
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+
+
+calculator()
